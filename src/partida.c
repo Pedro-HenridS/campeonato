39a4a9bdecf6c22408abc0d5886b4ab5595bf152
Luiz <luizhenriquebsantos17@gmail.com>
2025-11-07 17:10:45 -0300

    Mexendo no Partidas.C

---
+++ src/partida.c
@@ -0,0 +1,18 @@
+#include "partida.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+Partida* partida_cria(int id, int time1_id, int time2_id, int gols_time1, int gols_time2) {
+    Partida* nova_partida = (Partida*) malloc(sizeof(Partida));
+    if (nova_partida == NULL) {
+        perror("Erro ao alocar memória");
+        exit(EXIT_FAILURE);
+    }
+    nova_partida->id = id;
+    nova_partida->time1_id = time1_id;
+    nova_partida->time2_id = time2_id;
+    nova_partida->gols_time1 = gols_time1;
+    nova_partida->gols_time2 = gols_time2;
+    return nova_partida;
+
+}
