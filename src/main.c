9a3ed22c7980edd9fd619dc6938cb9e39f59a3bb
Luiz <luizhenriquebsantos17@gmail.com>
2025-11-07 18:00:18 -0300

    Ainda fazendo bd partida

--- src/main.c
+++ src/main.c
@@ -7,15 +7,15 @@ void _Menu_Principal(){
 char escolha;
 
 while (escolha != 'Q'){
-printf("Escolha uma opção\n");
-printf("1 - Consultar Time\n");
-printf("2 - Consultar Partida\n");
-printf("3 - Atualizar Partida\n");
-printf("4 - Remover Partida\n");
-printf("5 - Atualizar Partida\n");
-printf("6 - Imprimir tabela de classificação\n");
-printf("Q - Sair \n");
-scanf(" %c", &escolha);
+    printf("Escolha uma opção\n");
+    printf("1 - Consultar Time\n");
+    printf("2 - Consultar Partida\n");
+    printf("3 - Atualizar Partida\n");
+    printf("4 - Remover Partida\n");
+    printf("5 - Atualizar Partida\n");
+    printf("6 - Imprimir tabela de classificação\n");
+    printf("Q - Sair \n");
+    scanf(" %c", &escolha);
 
 switch(escolha){
     case '1': 
@@ -30,8 +30,7 @@ switch(escolha){
 }
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     _Menu_Principal();
     
     return 0;
