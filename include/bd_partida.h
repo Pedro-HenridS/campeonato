ef14b5d6dc0bf3e0dc4e817322376a2fd5cca77e
Pedro-HenridS <pedrosouza9479@gmail.com>
2025-11-06 23:11:38 -0300

    Definindo as funções para serem implementadas

--- include/bd_partida.h
+++ include/bd_partida.h
@@ -1,6 +1,20 @@
 #ifndef BD_PARTIDAS_H
 #define BD_PARTIDAS_H
 
-// typedef struct 
+#include <bd_time.h>
 
-#endif // BD_PARTIDAS_H
+typedef struct BDPartidas{
+
+}BDPartidas;
+
+BDPartidas* bd_partidas_cria(const char* nome_arquivo, BDTimes* bd_times);
+
+void bd_partidas_libera(BDPartidas* bd);
+
+void bd_partidas_consulta(
+    const BDPartidas* bd, 
+    const BDTimes* bd_times, 
+    const char* prefixo, 
+    int modo);
+
+#endif
--- include/partida.h
+++ include/partida.h
@@ -1,6 +1,8 @@
 #ifndef PARTIDAS_H
 #define PARTIDAS_H
 
+#include <bd_time.h>
+
 typedef struct Partida{
     int id;
     int time1_id;
@@ -10,8 +12,16 @@ typedef struct Partida{
 
 } Partida;
 
-Partida* create_match(){
+Partida* Partida_criar(){
     
 }
+
+Partida* partida_cria(int id, int time1_id, int time2_id, int gols_time1, int gols_time2);
+
+void partida_libera(Partida* p);
+
+void partida_processa_resultado(const Partida* p, BDTimes* bd_times);
+
+void partida_imprime_detalhes(const Partida* p, const BDTimes* bd_times);
 
 #endif
